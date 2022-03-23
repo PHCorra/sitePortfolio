87a75db9d5020fafb5dc4e6d82fafc9cd3436404
Pedro Corra <ph.corra@gmail.com>
2022-03-22 21:43:19 -0300

    Fixing the virtual environment and add static file for css

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
+import os
 from flask import Flask, render_template
+from flask_cors import CORS
 
 app = Flask(__name__)
 
+cors = CORS(app, resource={r"/*": {"origins": "*"}})
 # acess page
 # route -> endpoint, path
 
 # function -> o que você quer exibir naquela página
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def homepage():
     return render_template("index.html")
 
@@ -16,6 +19,12 @@ def homepage():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    pass
 
 
 # Running the code
